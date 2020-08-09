e8723ae819fd5ce56c74e5c0faa730a2a7ea7f97
Chien Huynh Van <mathenku1998@gmail.com>
2020-08-09 21:53:21 +0700

    react

---
+++ src/companents/todos.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function Todos() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <ul>
+            <li>
+                Go to market
+            </li>
+            <li>
+                Buy food
+            </li>
+            <li>
+                Make dinner
+            </li>
+        </ul>
+      </header>
+    </div>
+  );
+}
+
+export default Todos;
